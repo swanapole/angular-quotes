1d8b3e112acdaf94d4636a515eead07be6595bd4
swanapole <swanapoleshilla@yahoo.com>
2019-01-11 12:30:27 +0300

    add all the files

--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailsComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
